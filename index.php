2acc0dd48fc4b5c1d35009eae9505ba561b9a9d4
Tylor Dodge <dodget@wit.edu>
2015-04-02 12:19:31 -0400

    new stuff

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <body>
 
 	<form action="process.php" method="post">
-		Enter your number to receive a totally rad text! +1 <input name="number" type="number">
+		Enter your number to receive a totally rad text! +1 <input name="number" type="text">
 		<input type="submit">
 	
 	</form>
